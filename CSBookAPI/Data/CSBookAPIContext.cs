54c1cb0e05c72f9440af99cc36505e3c3d2c961b
Douglas Morais <douglasmorais9915@outlook.com>
2023-08-01 21:58:02 -0300

    Create project

---
+++ CSBookAPI/Data/CSBookAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CSBookAPI.Domain;
+
+namespace CSBookAPI.Data
+{
+    public class CSBookAPIContext : DbContext
+    {
+        public CSBookAPIContext (DbContextOptions<CSBookAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CSBookAPI.Domain.Book> Book { get; set; } = default!;
+    }
+}
---
+++ CSBookAPI/Domain/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace CSBookAPI.Domain
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Book(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
